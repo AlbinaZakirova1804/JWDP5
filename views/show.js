ff8e2e2077408e329ece386e00465d5ea37f2f62
Albina Zakirova <zak1804@gmail.com>
2020-02-07 15:52:54 -0500

    sent params through url and get them complite

--- views/show.js
+++ views/show.js
@@ -1,13 +1,23 @@
 
-//select elements on html
+//select elements placeholder on html
 
 const section = document.querySelector('section');
 const main = document.querySelector('main');
 
-//get json for particular teddy
+//get params _id for a single teddy from current url
+function getParameterByName(key, url) {
+   if (!url) url = window.location.href;
+   key = key.replace(/[\[\]]/g, '\\$&');
+   var regex = new RegExp('[?&]' + key + '(=([^&#]*)|&|#|$)'),
+       results = regex.exec(url);
+   if (!results) return null;
+   if (!results[2]) return '';
+   console.log(decodeURIComponent(results[2].replace(/\+/g, ' ')));
+   return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
 
-let dynamicURL = 'http://localhost:3000/api/teddies/5be9c8541c9d440000665243';
-
+//get json object for single teddy
+let dynamicURL = `http://localhost:3000/api/teddies/${getParameterByName('_id')}`;
 let newrequest = new XMLHttpRequest();
 
 newrequest.open('GET', dynamicURL);
@@ -19,78 +29,96 @@ newrequest.onload = function() {
 const teddy = newrequest.response; 
 showProduct(teddy); }
 
+// ***************build content on a page*****************
 function showProduct(jsonObj) {
    const product = jsonObj;
    console.log(product);
 
-   //div containers for two columns
+//create two div containers for two columns
    const aDiv = document.createElement('div');
    const bDiv = document.createElement('div');
-   
+
+// create content inside thoes containers
    const newP = document.createElement('p');
    const newH5 = document.createElement('h5');
    const newImg = document.createElement('img');
-   const newBut = document.createElement('button');
+
+// create colors dropdown
+   const addToCartBut = document.createElement('button');
    const dropBut = document.createElement('button');
+   const dropSpan = document.createElement('span');
    const dropDown = document.createElement('div');
-   const dropMenu = document.createElement('div');
-   const dropItem = document.createElement("li");
    const newUl = document.createElement('ul');
     
+//******************add text content***************/
    newH5.textContent = product.name;
     
    newP.textContent = product.description;
 
    newImg.src = product.imageUrl;
 
-   newBut.textContent = "add to cart";
+   addToCartBut.textContent = "add to cart";
 
    dropBut.textContent = 'available colors';
 
    
-
+// set attributes to classes
    section.setAttribute('class', 'row mx-2 my-5');
+
    aDiv.setAttribute('class', 'col-6');
    bDiv.setAttribute('class', 'col-6')
+
    newImg.setAttribute('class','img-responsive rounded w-100');
+   newImg.setAttribute('alt',"teddy bear "+product.name);
+
    dropDown.setAttribute('class', 'dropdown');
    dropBut.setAttribute('class', 'btn btn-secondary dropdown-toggle btn-sm');
-   dropItem.setAttribute('class', 'dropdown-item');
-   newImg.setAttribute('alt',"teddy bear "+product.name);
-   newBut.setAttribute('class', 'btn btn-primary');
+   dropBut.setAttribute("type", "button");
+   dropBut.setAttribute("data-toggle", "dropdown");
+
+   dropSpan.setAttribute('class', "caret");
+   
+   addToCartBut.setAttribute('class', 'btn btn-primary');
    newUl.setAttribute("class", "dropdown-menu");
     
-    
-    
-   
-   section.appendChild(aDiv);
-   section.appendChild(bDiv);
-   bDiv.appendChild(newH5);
+//***************built content on a page**************
+//section -> left container aDiv -> newImg -> div dropDown ->...
+//        -> right container bDiv -> newH5 -> addToCartBut button
+   section.appendChild(aDiv); 
+   section.appendChild(bDiv);    
+   //bDiv                          
+   bDiv.appendChild(newH5);   
    bDiv.appendChild(newP);
+   bDiv.appendChild(addToCartBut);
+   //aDiv
+   aDiv.appendChild(newImg);
    //building colors drop down
    aDiv.appendChild(dropDown);
    dropDown.appendChild(dropBut);
-   dropBut.appendChild(newUl);   
-  
+   dropBut.appendChild(dropSpan);
+   dropDown.appendChild(newUl);   
 
-  //colorsAvailable();
-   bDiv.appendChild(newBut);
-   aDiv.appendChild(newImg);
+   //aDiv.appendChild(newImg);
     console.log(product.colors);
     console.log(product.colors.length);
-    
-    for (var i=0; i<product.colors.length; i++){
+   
+   //building colors in drop down
+   for (var i=0; i<product.colors.length; i++){
        console.log(product.colors[i], i);
+       const dropItem = document.createElement("li");
+       dropItem.setAttribute('class', 'dropdown-item');
        dropItem.textContent = product.colors[i];
-       dropMenu.appendChild(dropItem);
+       newUl.appendChild(dropItem);
     }
+   //
    }
+//building local storage
 
-    
-function colors_avail(product) {
-  var i;
-   for (i=0; i<=product.colors.lenght; i++){
-      dropItem.textContent = product.color[i];
-      dropMenu.appendChild(dropItem);
-   }
-}
+// Listening to a click Event
+
+//listening to color drop down click Event
+var element = document.querySelectorAll('.dropItem');
+//addEventListener('click', function(){
+ //  window.localStorage.setItem('color', `${dropItem.textContent}`);
+//   console.log(localStorage);
+//;
