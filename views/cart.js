5be42a7d18ef3ed354cddce7cdcc5b2aa47c7345
Albina Zakirova <zak1804@gmail.com>
2020-02-21 19:15:48 -0500

    built basic cart view

--- views/cart.js
+++ views/cart.js
@@ -6,19 +6,28 @@ const main = document.querySelector('main');
 const section = document.querySelector('section');
 
 //creat content 
-const newDiv = document.createElement('div');
+const containerDiv = document.createElement('div');
 const cartHeadding = document.createElement('h1');
-const newP = document.createElement('p');
+//const nameDiv = document.createElement('div');
+//const colorDiv = document.createElement('div');
+//const priceDiv = document.createElement('div');
+//const placeHolder = document.createElement('div');
+
 
 //set attrivutes
 section.setAttribute('class', 'row mx-2 my-3 text-center');
-cartHeadding.setAttribute('class', 'text-center');
-newDiv.setAttribute('class', 'col-2');
+cartHeadding.setAttribute('class', 'text-center m-y-5');
+//outerDiv.setAttribute('class', 'col-4');
+
+cartHeadding.textContent = 'Welcome to your cart!';
+section.appendChild(cartHeadding);
 
 
+//built page
+//section.appendChild(itemDiv);
 
 //set content
-cartHeadding.textContent = 'Welcome to your cart!';
+//cartHeadding.textContent = 'Welcome to your cart!';
 //build the view of items in a cart
 if (localStorage.length == null)
          {
@@ -28,20 +37,61 @@ if (localStorage.length == null)
          {
             var retrievedData = localStorage.getItem("items");
             var items = JSON.parse(retrievedData);
+            //building cart content
             items.forEach((item) => {
                 console.log(item.name);
+                const itemDiv = document.createElement('div');
+                itemDiv.setAttribute('class', 'container row');
+                section.appendChild(itemDiv);
+//create name Div
+                const nameDiv = document.createElement('div');
+                        nameDiv.setAttribute('class','item-name col-3')
+                            const nameH6 =document.createElement('h6');
+                            nameH6.textContent = item.name;
+
+                     itemDiv.appendChild(nameDiv);
+                     nameDiv.appendChild(nameH6);
+                
+//create color Div
+                const colorDiv = document.createElement('div');
+                        colorDiv.setAttribute('class', 'item-color col-3');
+                            const colorH6 = document.createElement('h6');
+                            colorH6.textContent = item.color;
+
+                    itemDiv.appendChild(colorDiv);
+                    colorDiv.appendChild(colorH6);
+
+//create price Div
+                const priceDiv = document.createElement('div');
+                        priceDiv.setAttribute('class', 'item-color col-3');
+                            const priceH6 = document.createElement('h6');
+                            priceH6.textContent = '$'+item.price;
+
+                    itemDiv.appendChild(priceDiv);
+                    priceDiv.appendChild(priceH6);
+
+// create remove button
+                const placeHolderDiv = document.createElement('div');
+                        placeHolderDiv.setAttribute('class', 'item-remove col-3');
+                        const removeBut = document.createElement('button');
+                        removeBut.setAttribute('class', 'btn btn-link');
+                        removeBut.textContent = 'remove';
+
+                    itemDiv.appendChild(placeHolderDiv);
+                    placeHolderDiv.appendChild(removeBut);
+               
+//********************************************************************************** */
+             
 });
 
             console.log('I am a cart');
          };
 
 
-//built page
-main.appendChild(cartHeadding);
-section.appendChild(newDiv);
-newDiv.appendChild(newP);
+
+
 
 
 //for (var i = 0; storage.length < i; i++ ){
-    console.log(localStorage.getItem('items'));
+    //console.log(localStorage.getItem('items'));
 //}
