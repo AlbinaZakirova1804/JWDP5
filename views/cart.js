9597b22423aafe308ab45081c9b35ed3461cf790
Albina Zakirova <zak1804@gmail.com>
2020-03-16 15:36:52 -0400

    cleaned code in cart.js file

--- views/cart.js
+++ views/cart.js
@@ -1,7 +1,4 @@
-console.log(localStorage);
-
 //select elements placeholder on html
-
 const main = document.querySelector('main');
 const section = document.querySelector('section');
 
@@ -11,30 +8,24 @@ const wrapCartItemsInfo = document.createElement('div');
 const wrapCartItemsRemove = document.createElement('div');
 const cartHeadding = document.createElement('h1');
 
-
 //set attrivutes
 section.setAttribute('class', 'container mx-2 my-3 text-center');
-//cartHeadding.setAttribute('class', 'text-center m-y-5');
-//containerDiv.setAttribute('class', 'row');
 wrapContainerDiv.setAttribute('class', 'container mt-5 row');
 wrapCartItemsInfo.setAttribute('class', 'col-10');
 wrapCartItemsInfo.setAttribute('id', 'left');
 wrapCartItemsRemove.setAttribute('class', 'remove container col-2');
 wrapCartItemsRemove.setAttribute('id', 'right');
 
-
+//add content
 cartHeadding.textContent = 'Welcome to your cart!';
+
+//build layout
 section.appendChild(cartHeadding);
 section.appendChild(wrapContainerDiv);
 wrapContainerDiv.appendChild(wrapCartItemsInfo);
 wrapContainerDiv.appendChild(wrapCartItemsRemove);
 
-
-
-//set content
-
 //build the view of items in a cart
-
 if (localStorage.length === 0)
          {
             console.log('local storage is empty');
@@ -43,21 +34,21 @@ if (localStorage.length === 0)
          {
             var retrievedData = localStorage.getItem("items");
             var items = JSON.parse(retrievedData);
+
             //building cart content
-           
-            items.forEach((item) => {
+        items.forEach((item) => {
+                //preset layout
                 var indexInArray = items.indexOf(item);
-               // console.log(item.name);
                 const itemDiv = document.createElement('div');
                 itemDiv.setAttribute('class', 'container row mt-3');
                 itemDiv.setAttribute('id', 'div-'+indexInArray);
+                //build layout
                 wrapCartItemsInfo.appendChild(itemDiv);
 //create name Div
                 const nameDiv = document.createElement('div');
                 nameDiv.setAttribute('class','item-name col-3')
                 const nameH6 =document.createElement('h6');
                 nameH6.textContent = item.name;
-
                 itemDiv.appendChild(nameDiv);
                 nameDiv.appendChild(nameH6);
                 
@@ -91,41 +82,29 @@ if (localStorage.length === 0)
 //create remove button
                 const removeBut = document.createElement('button');
                 removeBut.setAttribute('class', 'btn btn-link mt-1 mb-1');
-                
                 removeBut.setAttribute('id', indexInArray );
                 removeBut.textContent = 'remove';
                 wrapCartItemsRemove.appendChild(removeBut);
-
-            }
-
-             
-            )
+        })
             totalCartPrice(items);
             createTotalCartPriceContent(items);
-            //validation, if cart empty dont we cant process it
+            //validation, if cart empty we can't process it
             createOrderButton();
         };
-
-           // totalCartPrice(items);
-           // createTotalCartPriceContent(items);
-            //validation, if cart empty dont we cant process it
-           // createOrderButton();
             
-            /**************************************************************/
-            var allRemoveButtons = document.querySelector("#right");
+/**********************listening for all****"remove" buttons click**************************/
+var allRemoveButtons = document.querySelector("#right");
 allRemoveButtons.addEventListener("click", removeItem, false);
 
 function removeItem(e){
-                if (e.target !== e.currentTarget) {
-                    var clickedButtonIndex = e.target.id;
-                    console.log(clickedButtonIndex);
-                   // alert("Item with id " + e.target.id + "was removed");
-                    retriveExistingLocalStorageData(items);
-                    console.log(items);
-                    deleteObjFromLocalStorage(items, clickedButtonIndex);
-                    /************************************************************/
-                    var elementDiv = document.getElementById(`div-${clickedButtonIndex}`);
-                    var elementBtn = document.getElementById(`${clickedButtonIndex}`);
+        if (e.target !== e.currentTarget) {
+                var clickedButtonIndex = e.target.id;
+                // alert("Item with id " + e.target.id + "was removed");
+                retriveExistingLocalStorageData(items);
+                deleteObjFromLocalStorage(items, clickedButtonIndex);
+                /************************************************************/
+                var elementDiv = document.getElementById(`div-${clickedButtonIndex}`);
+                var elementBtn = document.getElementById(`${clickedButtonIndex}`);
                     
                         elementDiv.remove();
                         elementBtn.remove();
@@ -133,16 +112,13 @@ function removeItem(e){
                         totalCartPrice(items);
                         updateCartPrice(items);
                         
-                    /************************************************************/
-                    console.log(localStorage);
+                /*********************************************************/
                 }
-                e.stopPropagation();
+        e.stopPropagation();
 }
 
 function deleteObjFromLocalStorage(items, objIndex) {
-        console.log('array items -> '+items+', obj index -> '+ objIndex);
         var removed = items.splice(objIndex, 1);
-        console.log('items was removed -> '+ removed +', items array after removal -> '+items);
         pushDataToLocalStorage(items);
 }
 
@@ -150,12 +126,9 @@ function totalCartPrice(items){
         let totalPrice = 0;
         retriveExistingLocalStorageData(items);
         items.forEach((item)=>{
-        console.log(item.price);
         totalPrice += item.price*item.qty 
-        console.log(totalPrice);
         return totalPrice;
         });
-        console.log('total cart price is '+totalPrice);
         return totalPrice;
 }
 
@@ -167,8 +140,6 @@ function totalCartPrice(items){
 
 function pushDataToLocalStorage(items){
         localStorage.setItem( 'items', JSON.stringify(items) );
-       // alert('item was added to the car');
-        console.log(localStorage);
 }
 
 function createTotalCartPriceContent(items){
@@ -193,26 +164,23 @@ function createTotalCartPriceContent(items){
 }
 
 
-
+//update total cart price after removing items out of cart
 function updateCartPrice(items){
-
         var div = document.getElementById('sum-price-container');
-       var h6 = document.getElementById('sum');
-        h6.remove();
-        const newTotalPrice = document.createElement('h6');
+        var h6 = document.getElementById('sum');
+        h6.remove(); //remove the old
+        const newTotalPrice = document.createElement('h6'); //create the new
         newTotalPrice.setAttribute('id', 'sum');
         newTotalPrice.textContent = 'Total price: $'+totalCartPrice(items);
         div.appendChild(newTotalPrice);
 //input validation on empty car order
         if (totalCartPrice(items) === 0){
-                document.getElementById('order-button-div').remove();
+                document.getElementById('order-button-div').remove();//remove order button for empty cart
                 
-        //set Local Storage empty to reduce error
+        //clean local storage
         localStorage.clear();
         }
 }
-
-
 
 function createOrderButton(){
         //get element on web page
